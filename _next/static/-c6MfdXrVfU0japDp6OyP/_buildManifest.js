6dc1de17dd3e335a9592c8427c99614c51c9830c
double-tap <help@double-tap.io>
2021-11-25 06:31:04 +0000

    2021-11-25 06:31:03 배포

---
+++ _next/static/-c6MfdXrVfU0japDp6OyP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/616-22d0d6de09a0ef09.js","static/chunks/pages/index-5fca74068a39c676.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
