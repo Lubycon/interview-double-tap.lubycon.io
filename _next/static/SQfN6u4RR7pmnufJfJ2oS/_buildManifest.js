082fc37050e32e2f097cdeee34ae565fc23026b0
double-tap <help@double-tap.io>
2021-11-25 08:48:56 +0000

    2021-11-25 08:48:54 배포

---
+++ _next/static/SQfN6u4RR7pmnufJfJ2oS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/706-a91f5c06e92c47c8.js","static/chunks/pages/index-aaf549f21b9bc4fe.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/fQrndpG3gYF03laSMRUzF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/351-9886703926175ba5.js","static/chunks/pages/index-0aae6709675c7842.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
